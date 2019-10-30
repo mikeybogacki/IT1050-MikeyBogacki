5c1b8c8ec59c386344fbf2bf051672dfe2ab1402
mikeybogacki <54599414+mikeybogacki@users.noreply.github.com>
2019-10-30 19:43:44 -0400

    Add files via upload

---
+++ Classwork/Class week 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inclassweek2
+{
+    class Car
+    {
+        // "public makes the values allowed in the Program" always set values
+        public string make;
+        public string model;
+        public string color;
+
+        public int year;
+        int speed;
+        int topspeed;
+
+        int accelerate()
+        {
+            speed = speed + 1;
+            return speed;
+        }
+    }
+}
