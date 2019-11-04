9a00565064a3debe7207889757c9036fa8a564a4
mikeybogacki <54599414+mikeybogacki@users.noreply.github.com>
2019-11-03 21:53:43 -0500

    Add files via upload

---
+++ Week2/AdditionProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace week2
+{
+     class Addition
+    {
+        public static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int sum;
+            Console.WriteLine("Enter First Integer:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Integer:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            sum = number2 + number1;
+            Console.WriteLine("The sum is " + sum);
+         
+
+        }
+    }
+}
---
+++ Week2/PartB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace week2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!")       Console.WriteLine("From Michael Bogacki");
+          
+
+
+
+
+        } 
+    }
+}
