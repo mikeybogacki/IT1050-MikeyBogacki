9bb4c2bad2927cf944af60190c07b7a5da36455d
mikeybogacki <54599414+mikeybogacki@users.noreply.github.com>
2019-11-06 20:50:19 -0500

    Add files via upload

---
+++ Week2/Product2Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int product;
+
+            Console.WriteLine("Enter first Integer");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Integer");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            product = number1 * number2;
+
+            Console.WriteLine("The product is " + product);
+
+        }
+    }
+}
