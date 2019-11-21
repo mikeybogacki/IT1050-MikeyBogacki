2f5b55095f4522f59950d466a6512744244567ba
mikeybogacki <54599414+mikeybogacki@users.noreply.github.com>
2019-11-20 19:41:16 -0500

    Add files via upload

---
+++ Classwork/classweek5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inclass5
+{
+    class myMath
+    {
+        public int operand1;
+        public int operand2;
+
+        public int addNumbers()
+        {
+            return operand1 + operand2;
+        }
+
+        public int addNumbers(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public int addNumbers(int num1, int num2)
+        {
+            int sum;
+            sum = num1 + num2;
+            return sum;
+
+        }
+        public int subtractNumbers(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int multiplyNumbers(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int divideNumbers(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Classwork/programweek5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace inclass5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myMath object1 = new myMath();
+
+           Console.WriteLine("The sum is " + object1.addNumbers(11, 10));
+           Console.WriteLine("The difference is " + object1.subtractNumbers(33, 21));
+           Console.WriteLine("The product is " + object1.multiplyNumbers(10, 20));
+            Console.WriteLine("The quotient is " + object1.divideNumbers(30, 15));
+
+            object1.operand1 = 10;
+            object1.operand2 = 9;
+
+            int sum;
+            // overloading addNumbers will output what is defined in class. and not mess with the other addNumber
+            sum = object1.addNumbers();
+            Console.WriteLine("the sum is " + sum);
+            Console.WriteLine("The sum is " + object1.addNumbers(40, 3, 9));
+
+
+        }
+    }
+}
