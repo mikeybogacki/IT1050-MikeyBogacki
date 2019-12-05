4c76e52ea9021e255deedda931e6f43b424fd652
mikeybogacki <54599414+mikeybogacki@users.noreply.github.com>
2019-12-04 19:31:27 -0500

    Add files via upload

---
+++ Classwork/class7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inclassweek6
+{
+    class MovieTest
+    {
+        static void Main(string[] args)
+        {
+            Movie myMovie = new Movie("Iron Man" , "Jon Favreau", 2008);
+            Movie myFavoriteMovie = new Movie("Rudy", 5, "David Anspaugh");
+            Movie myOtherMovie = new Movie("Scar Face");
+
+            myMovie.Display();
+            myFavoriteMovie.Display();
+            myOtherMovie.Display();
+
+
+
+        }                       
+
+               
+    }
+}
---
+++ Classwork/class7code.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inclassweek6
+{
+    class Movie
+    {
+        private string title;
+        private string director;
+        private int year;
+        private int rating;
+
+        
+        public Movie(string title, string director, int year)
+        {
+            this.title = title;
+            this.director = director;
+            this.year = year;
+            
+        }
+        public Movie(string title, int rating, string director)
+        {
+            this.title = title;
+            this.rating = rating;
+            this.director = director;
+        }
+        public Movie(string title)
+        {
+            this.title = title;            
+        }
+        
+        public void Display()
+        {
+            Console.WriteLine("{0} directed by {1} released in {2}\n" , title, director, year);
+            
+        }
+    
+
+
+
+
+    }
+}
